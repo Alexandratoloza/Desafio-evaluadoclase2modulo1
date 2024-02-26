22c572eeda2281343e6b6c354955035b9f7ec117
alexandra toloza rios <toloza.ale1992@gmail.com>
2024-02-26 16:39:28 -0300

    desafioiniciado

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+
+let Participante;
+let Compurador;
+function aleatoria(){
+    return Math.ceil(Math.random()*3)
+}
+Participante = prompt ("elija: 1 - piedra. 2 - papel, 3- tijera")
+Compurador = aleatoria();
+
+if(Participante === Compurador){
+    alert ("Empate 🫸🫸🫸")
+}
+else if (Participante ==1 && Compurador==3){
+    alert("Ganaste 🎉🎉🎉")
+
+}
+else if (Participante ==2 && Compurador==1){
+    alert("Ganaste 🎉🎉🎉")
+}
+else if (Participante ==3 && Compurador==2){
+    alert("Ganaste🎉🎉🎉")
+
+}
+
+else {
+    alert("oh no ¡Perdiste!😢😥 intentalo nuevamente.")
+
+}
